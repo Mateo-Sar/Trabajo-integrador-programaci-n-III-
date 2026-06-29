8bf11fd2a7270998e98f51fa0de35b80bda09ce1
Mateo Sartor <mateoresa@gmail.com>
2026-06-29 20:57:47 -0300

    Feat: se implemento validacion en formulario

--- script.js
+++ script.js
@@ -2,6 +2,17 @@
 const form = document.querySelector('form');
 const nombreUsuario = document.querySelector("#name");
 
+document.querySelector(".btn").addEventListener("click",()=> {
+  try{
+    if(document.querySelector("#texto").value.trim() === ""){
+      throw new Error("Por favor ingrese su opinion antes de enviar");
+    }
+  }
+  catch(e){
+    alert(e.message);
+  }
+})
+
 form.addEventListener('submit', function(event) {
   event.preventDefault(); 
   const name = nombreUsuario.value;
@@ -16,3 +27,4 @@ document.querySelector(".titulo").addEventListener("mouseenter",()=>{
 document.querySelector(".titulo").addEventListener("mouseleave",()=>{
   document.querySelector(".titulo").innerText = `Guia para el Juego "Skull King"`;
 })
+
