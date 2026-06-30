36a29e766556f3a39951b09694569254751b5082
Mateo Sartor <mateoresa@gmail.com>
2026-06-29 21:35:25 -0300

    Feat:implementacion de consejos

--- script.js
+++ script.js
@@ -28,3 +28,19 @@ document.querySelector(".titulo").addEventListener("mouseleave",()=>{
   document.querySelector(".titulo").innerText = `Guia para el Juego "Skull King"`;
 })
 
+const consejos = [`Evita predecir más bazas de las que realmente puedes controlar`,
+  `Usa tus cartas de "huida" para perder bazas intencionalmente`, 
+  `Mantente atento al ciclo de victorias`]
+
+  function mostrarConsejos(){
+    const lista = document.querySelector("#lista")
+    lista.innerHTML = " ";
+
+    for(let consejo of consejos) {
+        const item = document.createElement("li");
+        item.textContent = consejo
+        lista.appendChild(item); // agrega el elemento li a ul
+    }
+}
+
+document.querySelector("#mostrar").addEventListener("click",mostrarConsejos);
